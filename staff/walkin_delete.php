cb457454883da0b139682df3a70db5635a9f91e6
joyceongxr <joyceongxr@gmail.com>
2026-05-18 01:55:50 +0800

    fix

--- staff/walkin_delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once '../includes/staff_auth.php';
-require_once '../config/db.php';
-$id = intval($_GET['id']);
-$conn->query("DELETE FROM walkin_bookings WHERE id = $id");
-header("Location: walkin_bookings.php");
-exit;
-?>
